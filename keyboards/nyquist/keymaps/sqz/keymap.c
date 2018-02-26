bc141347b6f682864a780bf5e14e5127572581c6
squid <github@squiddity.com>
2018-02-25 20:30:32 -0800

    suppress adjust

--- keyboards/nyquist/keymaps/sqz/keymap.c
+++ keyboards/nyquist/keymaps/sqz/keymap.c
@@ -24,6 +24,9 @@ enum custom_keycodes {
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
+#define KC_LWR TG(_LOWER)
+#define KC_RSE TG(_RAISE)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -44,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT , \
-  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_RGHT \
+  KC_LCTL, KC_LALT, KC_LGUI, KC_LWR,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RSE,  KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 
@@ -119,6 +122,7 @@ void persistent_default_layer_set(uint16_t default_layer) {
   default_layer_set(default_layer);
 }
 
+/*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
@@ -147,14 +151,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-      /*    case ADJUST:
+      case ADJUST:
       if (record->event.pressed) {
         layer_on(_ADJUST);
       } else {
         layer_off(_ADJUST);
       }
       return false;
-      break;*/
+      break;
   }
   return true;
 }
+*/
