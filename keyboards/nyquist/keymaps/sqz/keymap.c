1fe67ead19e31af2d84799f63d51d38dab4cc972
squid <github@squiddity.com>
2018-03-07 08:35:05 -0800

    swap gui and alt

--- keyboards/nyquist/keymaps/sqz/keymap.c
+++ keyboards/nyquist/keymaps/sqz/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  |Lower |Space |Space |Space |Space |Raise | Left | Down |Right |
+ * | Ctrl | GUI  | Alt  |Lower |Space |Space |Space |Space |Raise | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   OSM_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   MOD_ENT , \
-  KC_LCTL, KC_LALT, KC_LGUI, OSL_LWR,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  OSL_RSE,  KC_LEFT, KC_DOWN, KC_RGHT \
+  KC_LCTL, KC_LGUI, KC_LALT, OSL_LWR,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  OSL_RSE,  KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 
