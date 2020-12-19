03f8d02e9e3a528be8f81c8f5a9d75c40ba5b098
alan <github@squiddity.com>
2020-12-19 11:56:44 -0800

    ee hands for iris

--- keyboards/keebio/iris/keymaps/sqz/config.h
+++ keyboards/keebio/iris/keymaps/sqz/config.h
@@ -27,9 +27,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
